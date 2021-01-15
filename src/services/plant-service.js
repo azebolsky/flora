fad5aff2c4aaad7d4eab9ed8e46cbd895bb82559
ADAM <zebolsky.adam@gmail.com>
2021-01-15 11:42:45 -0600

    update cors error in plant page

--- src/services/plant-service.js
+++ src/services/plant-service.js
@@ -1,7 +1,12 @@
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
 function getIndividualPlant(id) {
-  return fetch(
-    `https://trefle.io/api/v1/plants/${id}?token=ga9sPW6MBa8FDVSkKSWemxEqUJvgbKRNRiVYCSLZBms`
-  ).then((res) => res.json());
+  const url = `https://trefle.io/api/v1/plants/${id}?token=ga9sPW6MBa8FDVSkKSWemxEqUJvgbKRNRiVYCSLZBms`;
+  return fetch(proxyUrl + url, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
 }
 
 export { getIndividualPlant };
