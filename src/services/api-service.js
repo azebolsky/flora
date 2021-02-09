9f74d573fc3986046b5de704586b69730b520545
ADAM <zebolsky.adam@gmail.com>
2021-02-08 21:18:32 -0600

    update api call to fix cors issue

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -4,25 +4,27 @@ const API_TOKEN = process.env.REACT_APP_TREFLE_API_KEY;
 function getPlantsWithSearchAndPageNumber(page, search) {
   const url = `https://trefle.io/api/v1/plants/search?token=${API_TOKEN}&page=${page}&q=${search}`;
   return fetch(proxyUrl + url, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    accept: "application/json",
+    "Content-Type": "application/json",
   }).then((res) => res.json());
 }
 
 const getPlantsWithPageNumber = async (page) => {
   const url = `https://trefle.io/api/v1/plants?token=${API_TOKEN}&page=${page}`;
-  return await fetch(proxyUrl + url, {
-    accept: "application/json",
+  return await fetch(url, {
+    method: "GET",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   }).then((res) => res.json());
 };
 
 function getSpeciesWithPageNumber(page) {
   const url = `https://trefle.io/api/v1/species/678281?token=${API_TOKEN}&page=${page}`;
-  return fetch(proxyUrl + url, {
-    accept: "application/json",
+  return fetch(url, {
+    method: "GET",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    // accept: "application/json",
   }).then((res) => res.json());
 }
 
