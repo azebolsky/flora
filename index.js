6e1dd5b65740e7bf5b8f958fec3f7660c2e646fd
ADAM <zebolsky.adam@gmail.com>
2021-02-25 15:21:24 -0600

    add procfile for heroku deployment

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-// import * as functions from "firebase-functions";
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 require("dotenv").config();
-// import getPlants from "./routes/plants";
 
 const port = process.env.PORT || 3001;
 
@@ -15,21 +13,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "build")));
+
 const plantsRouter = require("./routes/plants");
 app.use("/api/plants", plantsRouter);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, function () {
   console.log("Runnning on " + port);
 });
-
-// app.get("**", (req, res) => {
-//   console.log("heyyyyyyyyyyyyy");
-//   getPlants().then((plants) => {
-//     const data = renderToString(plants);
-//     res.send(data);
-//   });
-// });
-
-// export let api = functions.https.onRequest(app);
 
 module.exports = app;
