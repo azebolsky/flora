21f590d524e7aed995937bdccba2364fc9f42b4a
ADAM <zebolsky.adam@gmail.com>
2021-02-25 15:28:17 -0600

    add path to backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 require("dotenv").config();
