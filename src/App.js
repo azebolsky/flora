abc87a1881939d607e50ce48c1681e099b418fa4
ADAM <zebolsky.adam@gmail.com>
2021-02-27 21:30:27 -0600

    test for error

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
       let parsedPlantData, allPages;
       if (typeof plantData === "string") {
         parsedPlantData = JSON.parse(plantData);
-        // allPages = Math.ceil(parsedPlantData.meta.total / 20);
+        allPages = Math.ceil(parsedPlantData.meta.total / 20);
       }
       // const parsedPlantData =
       //   typeof plantData === "string"
@@ -66,8 +66,8 @@ const App = () => {
       // const allPages = parsedPlantData
       //   ? Math.ceil(parsedPlantData.meta.total / 20)
       //   : "";
-      // setTotalPages(allPages);
       console.log(parsedPlantData);
+      setTotalPages(allPages);
       setItems(parsedPlantData.data);
     };
     return fetchData();
